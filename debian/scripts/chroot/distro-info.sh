944291a12ade351be62418a10f6c33c48df79561
shock-hamburger <shock.hamburger@gmail.com>
2025-06-04 22:53:56 -0500

    debian(default,distro-info): check in test script

---
+++ debian/scripts/chroot/distro-info.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo
+gcc -v
+echo
+
+################################################################################
+
+# This is not needed without "nocheck"
+apt install distro-info-data
+
+echo
+
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --help
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --all
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --devel
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --lts
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --stable
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --supported
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --unsupported
+LLVM_PROFILE_FILE="$(uuidgen).profraw" ./$EXECUTABLE --lts --date=2018-01-01
+
+echo
+
+################################################################################
+
+echo
+BINARY=$EXECUTABLE $THIS_DIR/common/get_coverage.sh
+echo
