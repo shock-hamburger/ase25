cefdf5be524ebf5a67a7c6b9e2ce6765bd2ceccf
shock-hamburger <shock.hamburger@gmail.com>
2025-06-10 01:11:14 -0500

    debian-diff.sh: fix ID calculation

--- diff/scripts/debian-diff.sh
+++ diff/scripts/debian-diff.sh
@@ -24,17 +24,20 @@ PREV_LOG_ID=$(ls $1-log/*.diff_log.txt 2>/dev/null | sort -n | tail -1)
 if [[ -z "$PREV_LOG_ID" ]]; then
     PREV_LOG_ID=0
 else
-    PREV_LOG_ID=$(basename $PREV_LOG_ID .txt)
+    PREV_LOG_ID=$(basename $PREV_LOG_ID .diff_log.txt)
 fi
 
 NEW_LOG_ID=$((PREV_LOG_ID + 1))
 
 if [[ $START_WITH != "diff" ]]; then
+
+    THIS_LOG_ID=$NEW_LOG_ID
+
     START_WITH=$START_WITH \
     INSTR_OPTION=gcc-mcdc \
     DEB_BUILD_OPTIONS="nocheck nostrip" \
     DEB_BUILD_PROFILES="nocheck nostrip" \
-    $DIFF_WORKDIR/ase25/debian/scripts/build-package.sh $1 |& tee $1-log/$NEW_LOG_ID.gcc_build_log.txt
+    $DIFF_WORKDIR/ase25/debian/scripts/build-package.sh $1 |& tee $1-log/$THIS_LOG_ID.gcc_build_log.txt
     RET=$?
     if [[ $RET -ne 0 ]]; then
         echo "GCC build-package.sh: $RET"
@@ -45,16 +48,19 @@ if [[ $START_WITH != "diff" ]]; then
     INSTR_OPTION=clang-mcdc \
     DEB_BUILD_OPTIONS="nocheck nostrip" \
     DEB_BUILD_PROFILES="nocheck nostrip" \
-    $DIFF_WORKDIR/ase25/debian/scripts/build-package.sh $1 |& tee $1-log/$NEW_LOG_ID.clang_build_log.txt
+    $DIFF_WORKDIR/ase25/debian/scripts/build-package.sh $1 |& tee $1-log/$THIS_LOG_ID.clang_build_log.txt
     RET=$?
     if [[ $RET -ne 0 ]]; then
         echo "Clang build-package.sh: $RET"
         exit 1
     fi
+else
+    THIS_LOG_ID=$PREV_LOG_ID
 fi
 
-if [[ $START_WITH == "diff" ]]; then
-    $THIS_DIR/../diff.py --csv-output $1-log/$PREV_LOG_ID.csv $DIFF_PY_OPTS deb $1 |& tee $1-log/$PREV_LOG_ID.diff_log.txt
-else
-    $THIS_DIR/../diff.py --csv-output $1-log/$NEW_LOG_ID.csv $DIFF_PY_OPTS deb $1 |& tee $1-log/$NEW_LOG_ID.diff_log.txt
-fi
+$THIS_DIR/../diff.py \
+--all-inconsistency-csv $1-log/$THIS_LOG_ID.inconsistent.csv \
+--total-num-csv $1-log/$THIS_LOG_ID.compared.csv \
+$DIFF_PY_OPTS \
+deb $1 |&\
+tee $1-log/$THIS_LOG_ID.diff_log.txt
